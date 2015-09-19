f10ae9c43f57f7de55c57e1f13d4dbf6a5f857ea
Yuki Izumi <yuki@kivikakk.ee>
2015-09-19 21:32:11 +1000

    Clean up memory.

--- c/engine.c
+++ c/engine.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct partial_nfa {
-    int c;
-    struct partial_nfa *o1, *o2;
-};
-
-typedef struct {
-    struct partial_nfa *entry;
-} regex_t;
-
 enum regex_token_type {
     ATOM,
     CONCAT,
@@ -211,25 +202,266 @@ void format_token(struct regex_token *token) {
     printf("\n");
 }
 
+#define CHAR_SPLIT 256
+#define CHAR_MATCH 257
+#define CHAR_MARK 258
+
+struct state {
+    int c;
+    struct state *o1, *o2;
+};
+
+struct state *state(int c, struct state *o1, struct state *o2) {
+    struct state *s = malloc(sizeof(*s));
+    s->c = c;
+    s->o1 = o1;
+    s->o2 = o2;
+    return s;
+}
+
+struct state matchstate = { CHAR_MATCH };
+
+void mark_states(struct state *s) {
+    if (!s || s == &matchstate) {
+        return;
+    }
+
+    s->c = CHAR_MARK;
+
+    if (s->o1 && s->o1->c == CHAR_MARK) {
+        s->o1 = NULL;
+    }
+
+    if (s->o2 && s->o2->c == CHAR_MARK) {
+        s->o2 = NULL;
+    }
+
+    mark_states(s->o1);
+    mark_states(s->o2);
+}
+
+void free_state_recursive(struct state *s) {
+    if (!s || s == &matchstate) {
+        return;
+    }
+
+    free_state_recursive(s->o1);
+    free_state_recursive(s->o2);
+    free(s);
+}
+
+void free_state(struct state *s) {
+    mark_states(s);
+
+    free_state_recursive(s);
+}
+
+struct ptrlist {
+    struct state **s;
+    struct ptrlist *next;
+};
+
+struct ptrlist *list1(struct state **s) {
+    struct ptrlist *l = malloc(sizeof(*l));
+    l->s = s;
+    l->next = NULL;
+    return l;
+}
+
+void patch(struct ptrlist *l, struct state *s) {
+    struct ptrlist *next;
+    for (struct ptrlist *i = l; i; i = next) {
+        next = i->next;
+        *i->s = s;
+        free(i);
+    }
+}
+
+struct ptrlist *append(struct ptrlist *l1, struct ptrlist *l2) {
+    struct ptrlist *oldl1 = l1;
+
+    while (l1->next) {
+        l1 = l1->next;
+    }
+    l1->next = l2;
+    return oldl1;
+}
+
+struct frag {
+    struct state *start;
+    struct ptrlist *out;
+    struct frag *prev;
+};
+
+typedef struct {
+    struct state *entry;
+} regex_t;
+
+struct frag *frag(struct state *start, struct ptrlist *out, struct frag *prev) {
+    struct frag *frag = malloc(sizeof(*frag));
+    frag->start = start;
+    frag->out = out;
+    frag->prev = prev;
+    return frag;
+}
+
+void push_frag(struct frag **stackp, struct state *start, struct ptrlist *out) {
+    *stackp = frag(start, out, *stackp);
+}
+
+struct frag pop_frag(struct frag **stackp) {
+    struct frag frag = **stackp;
+    free(*stackp);
+    *stackp = frag.prev;
+    frag.prev = NULL;
+    return frag;
+}
+
+struct state *token2nfa(struct regex_token *token) {
+    if (!token) {
+        return NULL;
+    }
+
+    struct frag *stackp = NULL,
+                e1, e2;
+    struct state *s;
+
+    for (; token; token = token->next) {
+        switch (token->type) {
+        case ATOM:
+            s = state(token->atom, NULL, NULL);
+            push_frag(&stackp, s, list1(&s->o1));
+            break;
+        case CONCAT:
+            e2 = pop_frag(&stackp);
+            e1 = pop_frag(&stackp);
+            patch(e1.out, e2.start);
+            push_frag(&stackp, e1.start, e2.out);
+            break;
+        case ALTERNATIVE:
+            e2 = pop_frag(&stackp);
+            e1 = pop_frag(&stackp);
+            s = state(CHAR_SPLIT, e1.start, e2.start);
+            push_frag(&stackp, s, append(e1.out, e2.out));
+            break;
+        case ZERO_MANY:
+            e1 = pop_frag(&stackp);
+            s = state(CHAR_SPLIT, e1.start, NULL);
+            patch(e1.out, s);
+            push_frag(&stackp, s, list1(&s->o2));
+            break;
+        case ONE_MANY:
+            e1 = pop_frag(&stackp);
+            s = state(CHAR_SPLIT, e1.start, NULL);
+            patch(e1.out, s);
+            push_frag(&stackp, e1.start, list1(&s->o2));
+            break;
+        case ZERO_ONE:
+            e1 = pop_frag(&stackp);
+            s = state(CHAR_SPLIT, e1.start, NULL);
+            push_frag(&stackp, s, append(e1.out, list1(&s->o2)));
+            break;
+        }
+    }
+
+    e1 = pop_frag(&stackp);
+    if (stackp) {
+        // TODO: free everything
+        return NULL;
+    }
+
+    patch(e1.out, &matchstate);
+    return e1.start;
+}
+
 regex_t *compile(char const *pattern) {
     struct regex_token *token = tokenise(pattern);
-    format_token(token);
+    struct state *state = token2nfa(token);
     free_token(token);
-
     regex_t *re = malloc(sizeof(*re));
+    re->entry = state;
     return re;
 }
 
+void free_regex(regex_t *re) {
+    free_state(re->entry);
+    free(re);
+}
+
+struct state_list {
+    struct state *s;
+    struct state_list *next;
+};
+
+void prepend(struct state_list **l, struct state *s) {
+    struct state_list *nl = malloc(sizeof(*nl));
+    nl->s = s;
+    nl->next = *l;
+    *l = nl;
+}
+
+void free_list(struct state_list *clist) {
+    while (clist) {
+        struct state_list *next = clist->next;
+        free(clist);
+        clist = next;
+    }
+}
+
+void addstate(struct state_list **l, struct state *s) {
+    if (!s){
+        return;
+    }
+
+    if (s->c == CHAR_SPLIT) {
+        addstate(l, s->o1);
+        addstate(l, s->o2);
+        return;
+    }
+
+    prepend(l, s);
+}
+
+void step(struct state_list *clist, int c, struct state_list **nlist) {
+    for (; clist; clist = clist->next) {
+        struct state *s = clist->s;
+        if (s->c == c) {
+            addstate(nlist, s->o1);
+        }
+    }
+}
+
 int match(regex_t *re, char const *s) {
+    struct state_list *clist = NULL;
+    prepend(&clist, re->entry);
+
+    for (; *s; ++s) {
+        struct state_list *nlist = NULL;
+        step(clist, *s, &nlist);
+        free_list(clist);
+        clist = nlist;
+    }
+
+    for (; clist; clist = clist->next) {
+        if (clist->s->c == CHAR_MATCH) {
+            free_list(clist);
+            return 1;
+        }
+    }
+
+    free_list(clist);
+
     return 0;
 }
 
 int main(int argc, char **argv) {
-    regex_t *re = compile("(ab)*d");
-    printf("ababd: %d\n", match(re, "ababd"));
-    printf("abd: %d\n", match(re, "abd"));
-    printf("d: %d\n", match(re, "d"));
-    printf("xd: %d\n", match(re, "xd"));
+    regex_t *re = compile("o(ab|xy)*d");
+    printf("oababd: %d\n", match(re, "oababd"));
+    printf("oxyabd: %d\n", match(re, "oxyabd"));
+    printf("oabd: %d\n", match(re, "oabd"));
+    printf("od: %d\n", match(re, "od"));
+    printf("oxd: %d\n", match(re, "oxd"));
+    free_regex(re);
     return 0;
 }
 
